bd5474700360aadfb8dbe86701a6149cd5c9c6a1
Sahil Lakhani <sahil.lakahni@tsttechnology.io>
2025-01-07 13:44:39 +0530

    Added files -> dataTypes.js, functions.js, logicalOperations.js

---
+++ dataTypes.js
@@ -0,0 +1,32 @@
+// integer
+let num = 10;
+
+// String
+let name = 'Sahil';
+
+// float
+let floatNum = 72.47;
+
+// alphabet
+let alphabet = 'a';
+
+// boolean
+let isEmpty = true;
+
+// Undefined -> declared but not given a value
+let age;
+
+// null -> declared but intentionally given a value null
+let obj = null;
+
+// Object
+let dog = {
+    name: 'Spike',
+    age: 10,
+
+    bark: () => {
+        console.log("Whoof Whoof");
+    }
+};
+
+dog.bark();
